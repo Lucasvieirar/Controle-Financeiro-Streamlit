1ac251677b8e60b758a4f3eba41550724a2eccb0
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-05-03 14:52:11 +0000

    acrescenta gráficos, organiza visualização e dados

--- main.py
+++ main.py
@@ -16,10 +16,25 @@ if file_upload:
     df = pd.read_csv(file_upload)
     df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y").dt.date
 
+    exp1 = st.expander("Dados Brutos")
     columns_fmt = {"valor":st.column_config.NumberColumn("valor", format="$%d")}
+    exp1.dataframe(df, hide_index=True, column_config=columns_fmt)
 
-    st.dataframe(df, hide_index=True, column_config=columns_fmt)
-
-
+    exp2 = st.expander("Instituições")
     df_instituicao = df.pivot_table(index="Data", columns="Instituição", values="Valor")
-    st.dataframe(df_instituicao)
+    tab_data, tab_history, tab_share = exp2.tabs(["Dados", "Histórico", "Distribuição"])
+    with tab_data:
+        st.dataframe(df_instituicao)
+
+    with tab_history:
+        st.line_chart(df_instituicao)
+
+    with tab_share:
+
+        date = st.date_input("Data para distribuição", min_value=df_instituicao.index.min(),
+                      max_value=df_instituicao.index.max() )
+
+        if date not in df_instituicao.index:
+            st.warning("Entre com uma data válida")
+        else:
+            st.bar_chart(df_instituicao.loc[date])
