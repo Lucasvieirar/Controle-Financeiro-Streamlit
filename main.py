830bef3602bc3c4a3083d7a3609171e981cb44b3
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-05-03 14:21:53 +0000

    formata data e coloca visualização de instituição

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ Solução para organização financeira
 )
 file_upload = st.file_uploader(label="faça upload dos dados", type=["csv"])
 if file_upload:
+
     df = pd.read_csv(file_upload)
+    df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y").dt.date
+
     columns_fmt = {"valor":st.column_config.NumberColumn("valor", format="$%d")}
+
     st.dataframe(df, hide_index=True, column_config=columns_fmt)
+
+
+    df_instituicao = df.pivot_table(index="Data", columns="Instituição", values="Valor")
+    st.dataframe(df_instituicao)
